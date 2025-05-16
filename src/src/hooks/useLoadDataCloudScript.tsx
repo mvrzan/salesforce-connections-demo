4d7fb55ba442a497ad454b9e41c351f1e8b83d99
mvrzan <mvrzan@icloud.com>
2025-05-16 15:33:21 -0400

    add custom hook for loading Salesforce data cloud script

---
+++ src/src/hooks/useLoadDataCloudScript.tsx
@@ -0,0 +1,40 @@
+import { writeToLocalStorage } from "../utils/localStorageUtil";
+
+const useDataCloudScript = () => {
+  const configureScriptUrl = (url: string) => {
+    if (!url) return;
+
+    const existingScript = document.querySelector('script[src*="c360a.min.js"]');
+    if (existingScript) {
+      existingScript.remove();
+    }
+
+    const script = document.createElement("script");
+    script.src = url;
+
+    script.onload = async () => {
+      try {
+        await window.SalesforceInteractions.init({
+          consents: [
+            {
+              provider: "Einstein Personalization Demo",
+              purpose: window.SalesforceInteractions.ConsentPurpose.Tracking,
+              status: window.SalesforceInteractions.ConsentStatus.OptIn,
+            },
+          ],
+        });
+        console.log("Salesforce Interactions initialized");
+      } catch (error) {
+        console.error("Salesforce Interactions failed to initialize", error);
+      }
+    };
+
+    document.head.appendChild(script);
+
+    writeToLocalStorage("scriptUrl", url);
+  };
+
+  return configureScriptUrl;
+};
+
+export default useDataCloudScript;
