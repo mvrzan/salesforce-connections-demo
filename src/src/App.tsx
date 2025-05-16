08a3194be2a26b93eeeca6910bfa390c254ba1d2
mvrzan <mvrzan@icloud.com>
2025-05-16 15:33:29 -0400

    add data cloud script configuration in App component

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,8 +1,15 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
+import useDataCloudScript from "./hooks/useLoadDataCloudScript";
 
 function App() {
+  const configureScriptUrl = useDataCloudScript();
+  const dataCloudWebSdkUrl = import.meta.env.VITE_DATA_CLOUD_WEB_SDK_URL;
+
+  console.log("dataCloudWebSdkUrl", dataCloudWebSdkUrl);
+
+  configureScriptUrl(dataCloudWebSdkUrl);
   return (
     <>
       <Header />
