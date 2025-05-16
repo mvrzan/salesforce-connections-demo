9eb857b6570b9f1435c16a338f1399e4b0d982c6
mvrzan <mvrzan@icloud.com>
2025-05-16 14:49:42 -0400

    add HeroSection component

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,10 +1,12 @@
-import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <HeroSection />
       <Footer />
     </>
   );
